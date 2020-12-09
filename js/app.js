147ec90e3a1d280a7af21d6414a7d0265e804cb1
Andrei <webconsult.ekb@gmail.com>
2020-12-09 18:01:57 +0200

    pwa deployed

--- js/app.js
+++ js/app.js
@@ -150,3 +150,13 @@ const initDash = () => {
  * Старт приложения
  */
 initDash();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log('Service worker registration failed, error:', error);
+  });
+}
