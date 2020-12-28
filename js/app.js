844ab6afa446fc0bfa4cb0cfae9b24f61318bba3
ataztech910 <webconsult.ekb@gmail.com>
2020-12-28 12:56:31 +0100

    1 file modified

--- js/app.js
+++ js/app.js
@@ -158,9 +158,12 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("/service-worker.js")
     .then(function(registration) {
-      console.log("Registration successful, scope is:", registration.scope);
+      console.log(
+        "Регистрация успешна. Область видимости:",
+        registration.scope
+      );
     })
     .catch(function(error) {
-      console.log("Service worker registration failed, error:", error);
+      console.log("Сервис не включен, ошибка:", error);
     });
 }
